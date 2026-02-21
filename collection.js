af27a29f8eb2804fe9fc720d12af25d04ad150d3
Karthik27345 <karthikvijaysuresh@gmail.com>
2026-02-21 17:12:50 +0530

    Updated responsive layout and filtering

--- collection.js
+++ collection.js
@@ -24,79 +24,46 @@ closer.addEventListener("click",()=>{
 const products = document.querySelectorAll(".product")
 const filters = document.querySelectorAll("input[type='checkbox']")
 
-filters.forEach(filter=>{
-  filter.addEventListener("change",filterproducts)
-})
+filters.forEach(f=>
+  f.addEventListener("change",filterproducts)
+)
 
 function filterproducts(){
-  const selectedoccasion = []
-  const selectedcolors = []
-  const selectedarrivals = []
-  if(document.getElementById("summer").checked){
-    selectedoccasion.push("summer")
+  const activefilters = {
+    color:[],
+    arrival:[],
+    occasion:[]
+  }
+  
+  
+filters.forEach(f=>{
+  if(f.checked){
+    const type = f.dataset.filter
+    activefilters[type].push(f.value)
   }
 
-  if(document.getElementById("party").checked){
-    selectedoccasion.push("party")
+})
+
+products.forEach(product=>{
+
+  const color = product.dataset.color
+  const arrival = product.dataset.arrival
+  const occasion = product.dataset.occasion
+
+  const matchColor = activefilters.color.length==0||activefilters.color.includes(color)
+
+  const matcharrival = activefilters.arrival.length==0||activefilters.arrival.includes(arrival)
+
+  const matchoccasion = activefilters.occasion.length==0||activefilters.occasion.includes(occasion)
+
+  if(matchColor&&matcharrival&&matchoccasion){
+    product.style.display = "block"
   }
 
-  if(document.getElementById("beach").checked){
-    selectedoccasion.push("beach")
+  else{
+    product.style.display = "none"
   }
 
-  if(document.getElementById("red").checked){
-    selectedcolors.push("red")
-  }
+})
 
-  if(document.getElementById("white").checked){
-   selectedcolors.push("white")
-  }
-
-    if(document.getElementById("green").checked){
-    selectedcolors.push("green")
-  }
-
-    if(document.getElementById("blue").checked){
-  selectedcolors.push("blue")
-  }
-
-    if(document.getElementById("new").checked){
-    selectedarrivals.push("new")
-  }
-
-    if(document.getElementById("old").checked){
-    selectedarrivals.push("old")
-  }
-
-  products.forEach(product=>{
-    const occasion = product.dataset.occasion
-    const colors = product.dataset.color
-    const arrivals = product.dataset.arrival
-
-    if(selectedoccasion.length==0||selectedoccasion.includes(occasion)){
-      product.style.display = "block"
-    }
-
-    else{
-      product.style.display = "none"
-    }
-
- if(selectedcolors.length==0||selectedcolors.includes(colors)){
-      product.style.display = "block"
-    }
-
-    else{
-      product.style.display = "none"
-    }
-
-     if(selectedarrivals.length==0||selectedarrivals.includes(arrivals)){
-      product.style.display = "block"
-    }
-
-    else{
-      product.style.display = "none"
-    }
-
-
-  })
 }
